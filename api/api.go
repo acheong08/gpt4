4184bb2402005c5d1bfb441c5d60501477e3358a
Antonio Cheong <acheong@student.dalat.org>
2023-03-17 02:20:10 +0800

    remove ORG

--- api/api.go
+++ api/api.go
@@ -12,7 +12,6 @@ import (
 
 var (
 	key         = os.Getenv("OPENAI_KEY")
-	org         = os.Getenv("OPENAI_ORG")
 	endpoint    = os.Getenv("OPENAI_ENDPOINT")
 	http_client = http.DefaultClient
 )
@@ -21,7 +20,6 @@ func newRequest(body io.Reader) *http.Request {
 	base_request, _ := http.NewRequest("POST", endpoint, body)
 	base_request.Header.Add("Authorization", "Bearer "+key)
 	base_request.Header.Add("Content-Type", "application/json")
-	base_request.Header.Add("OpenAI-Organization", org)
 	base_request.Header.Add("Accept", "application/json")
 	base_request.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
 	return base_request
